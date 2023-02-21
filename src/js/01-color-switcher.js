0aa6f95dbb392e236b292b3326bac723eaee4d43
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-02-21 13:47:27 +0200

    /

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+let intervalId;
+let currentColor;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+
+  intervalId = setInterval(() => {
+    currentColor = getRandomHexColor();
+    body.style.backgroundColor = currentColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+
+  clearInterval(intervalId);
+
+  if (currentColor) {
+    body.style.backgroundColor = currentColor;
+  } else {
+    body.style.backgroundColor = '';
+  }
+});
